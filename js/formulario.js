d839171b4d2f4cc5eec072962f8dd949ea96458b
Flavia Costa <flavinha.goncalves@gmail.com>
2023-05-13 16:45:41 -0300

    crud-editar: Criada função de lógica ao clicar nos botões editar e adicionar

--- js/formulario.js
+++ js/formulario.js
@@ -1,16 +1,36 @@
 // Url Api
 const url = 'https://nodejs-production-74ce.up.railway.app/user';
+
+// Adicionar Usuário
 const formUsuario = document.querySelector('#formulario');
-formUsuario.addEventListener('submit', function (event) {
-    event.preventDefault();
-    const nome = document.querySelector('#nome').value;
-    const sobrenome = document.querySelector('#sobrenome').value;
-    const idade = document.querySelector('#idade').value;
-    const email = document.querySelector('#email').value;
-    const senha = document.querySelector('#senha').value;
-    console.log(nome, sobrenome, idade, email, senha); // check if form data is captured
-    addUsuario(nome, sobrenome, idade, email, senha);
-})
+const id = window.location.href.split('?')[1];
+
+if (id != null) {
+    preencherFormulario(id);
+    formUsuario.addEventListener('submit', function (event) {
+        event.preventDefault();
+        const nome = document.querySelector('#nome').value;
+        const sobrenome = document.querySelector('#sobrenome').value;
+        const idade = document.querySelector('#idade').value;
+        const email = document.querySelector('#email').value;
+        const senha = document.querySelector('#senha').value;
+        console.log(nome, sobrenome, idade, email, senha); // Verificar se os dados do formulário foram capturados
+        editarUsuario(id, nome, sobrenome, idade, email, senha);
+    });
+} else {
+    formUsuario.addEventListener('submit', function (event) {
+        event.preventDefault();
+        const nome = document.querySelector('#nome').value;
+        const sobrenome = document.querySelector('#sobrenome').value;
+        const idade = document.querySelector('#idade').value;
+        const email = document.querySelector('#email').value;
+        const senha = document.querySelector('#senha').value;
+        console.log(nome, sobrenome, idade, email, senha); // check if form data is captured
+        addUsuario(nome, sobrenome, idade, email, senha);
+    })
+}
+
+
 function addUsuario(nome, sobrenome, idade, email, senha) {
     fetch(url, {
         method: 'POST',
@@ -38,46 +58,86 @@ function addUsuario(nome, sobrenome, idade, email, senha) {
         .catch(error => console.error(error));
 }
 
-    // Obter referências aos elementos do formulário
-    const inputNome = document.querySelector('#nome');
-    const inputSobrenome = document.querySelector('#sobrenome');
-    const inputIdade = document.querySelector('#idade');
-    const inputEmail = document.querySelector('#email');
-    const inputSenha = document.querySelector('#senha');
+//Criado botão cancelar caso o usuário deseje voltar para a página principal
+const btnCancelar = document.querySelector('#btn-cancelar');
+btnCancelar.addEventListener('click', function () {
+    window.location.href = 'main.html';
+})
 
-    const id = window.location.href.split('?')[1];
-    if(id != null){
-        preencherFormulario(id);
-    }
+//Função editar o usuário
 
-    async function buscarUsuario(id){
-        const url = `https://nodejs-production-74ce.up.railway.app/user/${id}`;
-        const response = await fetch(url);
-        return await response.json();
-    }
-    
-    function preecherCampos(usuario){
-        inputNome.value = usuario.nome;
-        inputNome.classList.add('valid');
-        document.querySelector('#labelNome').classList.add('active');
-        inputSobrenome.value = usuario.sobrenome;
-        inputNome.classList.add('valid');
-        document.querySelector('#labelSobrenome').classList.add('active');
-        inputIdade.value = usuario.idade;
-        inputNome.classList.add('valid');
-        document.querySelector('#labelIdade').classList.add('active');
-        inputEmail.value = usuario.email;
-        inputNome.classList.add('valid');
-        document.querySelector('#labelEmail').classList.add('active');
-        inputSenha.value = usuario.senha;
-        inputNome.classList.add('valid');
-        document.querySelector('#labelSenha').classList.add('active');
-        
-    }
+// Obter referências aos elementos do formulário
+const inputNome = document.querySelector('#nome');
+const inputSobrenome = document.querySelector('#sobrenome');
+const inputIdade = document.querySelector('#idade');
+const inputEmail = document.querySelector('#email');
+const inputSenha = document.querySelector('#senha');
 
-    async function preencherFormulario(id){
-        const usuario = await buscarUsuario(id);
-        preecherCampos(usuario);
-    }
-   
+
+async function buscarUsuario(id) {
+    const url = `https://nodejs-production-74ce.up.railway.app/user/${id}`;
+    const response = await fetch(url);
+    return await response.json();
+}
+
+function preecherCampos(usuario) {
+    inputNome.value = usuario.nome;
+    inputNome.classList.add('valid');
+    document.querySelector('#labelNome').classList.add('active');
+    inputSobrenome.value = usuario.sobrenome;
+    inputNome.classList.add('valid');
+    document.querySelector('#labelSobrenome').classList.add('active');
+    inputIdade.value = usuario.idade;
+    inputNome.classList.add('valid');
+    document.querySelector('#labelIdade').classList.add('active');
+    inputEmail.value = usuario.email;
+    inputNome.classList.add('valid');
+    document.querySelector('#labelEmail').classList.add('active');
+    inputSenha.value = usuario.senha;
+    inputNome.classList.add('valid');
+    document.querySelector('#labelSenha').classList.add('active');
+
+}
+
+async function preencherFormulario(id) {
+    const usuario = await buscarUsuario(id);
+    preecherCampos(usuario);
+}
+
+//Validação de dados (inputs)
+
+//Salvar os dados novos do usuário
+function editarUsuario(id, nome, sobrenome, idade, email, senha) {
+    fetch(`${url}/${id}`, {
+        method: 'PUT',
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            nome,
+            sobrenome,
+            idade,
+            email,
+            senha,
+        }
+
+        )
+    })
+        .then(response => {
+            if (response.ok) {
+                alert('Dados Atualizados com sucesso!')
+                window.location.replace('./main.html');
+            } else {
+                alert('Ocorreu um erro, tente novamente!')
+            }
+        })
+        .then(error => {
+            console.error('Erro de requisição:', error);
+        })
+
+}
+
+
+
+
 
